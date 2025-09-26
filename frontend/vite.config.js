c245bb5309866aada9c0ab4b7e61435b58863294
Shivam <shivamkrsingh1911@gmail.com>
2025-09-26 22:09:39 +0530

    feat: Configure frontend (Vite, Tailwind) and backend (Next.js) projects

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    react({
+      include: "**/*.{jsx,js}",
+    }),
+  ],
+  // Use the more robust object syntax for the proxy
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true, // This is often necessary
+        secure: false,
+      },
+    },
+  },
+});
